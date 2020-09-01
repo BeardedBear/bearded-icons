5e37e74136162279f3b833ca3b5c49eb96720c51
BeardedBear <germain.poirrier@gmail.com>
2020-09-01 13:06:00 +0200

    First iteration of build system.

---
+++ src/icons.js
@@ -0,0 +1,191 @@
+
+const icons = {
+  "_file": {
+    "iconPath": "./icons/default.svg"
+  },
+  "_file_light": {
+    "iconPath": "./icons/default-light.svg"
+  },
+  "_folder": {
+    "iconPath": "./icons/folder.svg"
+  },
+  "_folder_light": {
+    "iconPath": "./icons/folder-light.svg"
+  },
+  "_folder_open": {
+    "iconPath": "./icons/folder-open.svg"
+  },
+  "_folder_light_open": {
+    "iconPath": "./icons/folder-light-open.svg"
+  },
+  "_f_elm": {
+    "iconPath": "./icons/elm.svg"
+  },
+  "_f_scss": {
+    "iconPath": "./icons/scss.svg"
+  },
+  "_f_docker": {
+    "iconPath": "./icons/docker.svg"
+  },
+  "_f_git": {
+    "iconPath": "./icons/git.svg"
+  },
+  "_f_readme": {
+    "iconPath": "./icons/readme.svg"
+  },
+  "_f_html": {
+    "iconPath": "./icons/html.svg"
+  },
+  "_f_json": {
+    "iconPath": "./icons/json.svg"
+  },
+  "_f_markdown": {
+    "iconPath": "./icons/markdown.svg"
+  },
+  "_f_css": {
+    "iconPath": "./icons/css.svg"
+  },
+  "_f_cssmap": {
+    "iconPath": "./icons/cssmap.svg"
+  },
+  "_f_js": {
+    "iconPath": "./icons/js.svg"
+  },
+  "_f_jsmap": {
+    "iconPath": "./icons/jsmap.svg"
+  },
+  "_f_typescript": {
+    "iconPath": "./icons/ts.svg"
+  },
+  "_f_typescriptdef": {
+    "iconPath": "./icons/tsdef.svg"
+  },
+  "_f_vue": {
+    "iconPath": "./icons/vue.svg"
+  },
+  "_f_image": {
+    "iconPath": "./icons/img.svg"
+  },
+  "_f_zip": {
+    "iconPath": "./icons/archive.svg"
+  },
+  "_f_yarn": {
+    "iconPath": "./icons/yarn.svg"
+  },
+  "_f_npm": {
+    "iconPath": "./icons/npm.svg"
+  },
+  "_f_svg": {
+    "iconPath": "./icons/svg.svg"
+  },
+  "_f_yaml": {
+    "iconPath": "./icons/yaml.svg"
+  },
+  "_f_font": {
+    "iconPath": "./icons/font.svg"
+  },
+  "_f_php": {
+    "iconPath": "./icons/php.svg"
+  },
+  "_f_twig": {
+    "iconPath": "./icons/twig.svg"
+  },
+  "_f_composer": {
+    "iconPath": "./icons/composer.svg"
+  },
+  "_f_symfony": {
+    "iconPath": "./icons/symfony.svg"
+  },
+  "_f_xml": {
+    "iconPath": "./icons/xml.svg"
+  },
+  "_f_robots": {
+    "iconPath": "./icons/robot.svg"
+  },
+  "_f_bundler": {
+    "iconPath": "./icons/gem.svg"
+  },
+  "_f_stylelint": {
+    "iconPath": "./icons/stylelint.svg"
+  },
+  "_f_prettier": {
+    "iconPath": "./icons/prettier.svg"
+  },
+  "_f_python": {
+    "iconPath": "./icons/python.svg"
+  },
+  "_f_log": {
+    "iconPath": "./icons/log.svg"
+  },
+  "_f_license": {
+    "iconPath": "./icons/license.svg"
+  },
+  "_f_audio": {
+    "iconPath": "./icons/audio.svg"
+  },
+  "_f_lua": {
+    "iconPath": "./icons/lua.svg"
+  },
+  "_f_csharp": {
+    "iconPath": "./icons/csharp.svg"
+  },
+  "_f_dartlang": {
+    "iconPath": "./icons/dart.svg"
+  },
+  "_f_eslint": {
+    "iconPath": "./icons/eslint.svg"
+  },
+  "_f_handlebars": {
+    "iconPath": "./icons/handlebars.svg"
+  },
+  "_f_webpack": {
+    "iconPath": "./icons/webpack.svg"
+  },
+  "_f_ruby": {
+    "iconPath": "./icons/ruby.svg"
+  },
+  "_f_jest": {
+    "iconPath": "./icons/jest.svg"
+  },
+  "_f_babel": {
+    "iconPath": "./icons/babel.svg"
+  },
+  "_f_toml": {
+    "iconPath": "./icons/toml.svg"
+  },
+  "_f_elixir": {
+    "iconPath": "./icons/elixir.svg"
+  },
+  "_f_rust": {
+    "iconPath": "./icons/rust.svg"
+  },
+  "_f_reactjs": {
+    "iconPath": "./icons/react.svg"
+  },
+  "_f_angular": {
+    "iconPath": "./icons/angular.svg"
+  },
+  "_f_rollup": {
+    "iconPath": "./icons/rollup.svg"
+  },
+  "_f_java": {
+    "iconPath": "./icons/java.svg"
+  },
+  "_f_gulp": {
+    "iconPath": "./icons/gulp.svg"
+  },
+  "_f_grunt": {
+    "iconPath": "./icons/grunt.svg"
+  },
+  "_f_editorconfig": {
+    "iconPath": "./icons/editorconfig.svg"
+  },
+  "_f_light_editorconfig": {
+    "iconPath": "./icons/editorconfig-light.svg"
+  },
+  "_f_shell": {
+    "iconPath": "./icons/shell.svg"
+  }
+}
+
+export default icons;
