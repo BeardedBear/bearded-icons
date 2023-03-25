d514ee4e1e1018f0a3da4c706645ce05f391c371
BeardedBear <germain.poirrier@gmail.com>
2023-03-25 09:32:35 +0100

    Add .yarn.installed file icon

--- src/shared/fileNames.ts
+++ src/shared/fileNames.ts
@@ -483,6 +483,7 @@ export default {
   ".yaspeller.json": "_f_yandex",
   "yarn.lock": "_f_yarn",
   ".yarnrc": "_f_yarn",
+  ".yarn.installed": "_f_yarn",
   ".yarnclean": "_f_yarn",
   ".yarn-integrity": "_f_yarn",
   ".yarn-metadata.json": "_f_yarn",
